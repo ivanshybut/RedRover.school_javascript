eae56417a860e333f43b7bd3dea7f2cb1ea3c72e
Ivan Shybut <ivanshybut92@gmail.com>
2023-04-03 21:39:09 +0300

    function update

--- qa_javascript/function.js
+++ qa_javascript/function.js
@@ -1,1 +1,15 @@
 // Function
+
+// Function declaration
+function name (param1, param2){
+    statements
+return expression
+}
+
+// Function expression
+let name = function (param1, param2) {
+    statements
+return expression
+}
+
+// Arrow Function

d28b834878dfcdf9fb58350daae30026fd34cf42
Ivan Shybut <ivanshybut92@gmail.com>
2023-04-04 18:19:21 +0300

    declaration func

--- qa_javascript/function.js
+++ qa_javascript/function.js
@@ -1,15 +1,39 @@
 // Function
 
 // Function declaration
-function name (param1, param2){
-    statements
-return expression
-}
+// function name (param1, param2){
+//     statements
+// return expression
+// }
 
-// Function expression
-let name = function (param1, param2) {
-    statements
-return expression
-}
+// // Function expression
+// let name = function (param1, param2) {
+//     statements
+// return expression
+// }
 
 // Arrow Function
+// let name = (arg1, arg2, ...) => expression;
+
+//===============function declaration
+//1. Слово function
+//2. Имя функции 
+//3. Список параметров в круглых скобках через запятую
+//4. Код функции ("Тело функции"), внутри фигурных скобках
+
+
+function sum (a, b) {
+    let result = a + b
+    console.log(result); 
+}
+sum (3, 6)
+
+function sum2 (a1, b1) {
+    let result = a1 + b1
+    return result
+}
+console.log(sum(2,3))
+
+//
+
+

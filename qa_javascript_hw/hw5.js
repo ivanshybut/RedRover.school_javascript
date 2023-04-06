cb4685feaf3d73a11a260dce891a329bcdb5d187
Ivan Shybut <ivanshybut92@gmail.com>
2023-04-06 21:35:34 +0300

    coffee + function hw

--- qa_javascript_hw/hw5.js
+++ qa_javascript_hw/hw5.js
@@ -1,1 +1,24 @@
 //coffee + function
+
+function showMeWelcomeMassage (){
+    console.log('Welcome to our self-service cafe!');
+    console.log('We have different cup sizes. Please, choose one:\n');
+}
+
+function showCupOptions (){
+   let smallCup = 'small cup'
+   let mediumCup = 'medium cup'
+   let largeCup = 'large cup'
+   let price = 5
+   
+   let cupsOptions = [[smallCup, price], [mediumCup, (price * 1.5), [largeCup, price * 2]]]
+
+   for (let i = 0; i < cupsOptions.length; i ++){
+    console.log(cupsOptions[i][0] + ' - ' + cupsOptions[i][1] + '$');
+
+    if (i == cupsOptions.length -1){
+        console.log('\n');
+    }
+   }
+}
+    

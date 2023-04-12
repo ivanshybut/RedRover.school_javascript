c29eba9e6afa2b99fa970eda838ac2b8a7adc29b
Ivan Shybut <ivanshybut92@gmail.com>
2023-04-12 19:14:30 +0300

    update

--- qa_javascript/stringAndArrayMethods.js
+++ qa_javascript/stringAndArrayMethods.js
@@ -1,1 +1,49 @@
 // Методы pop/push, shift/unshift
+//push - добавляет элемент(ы) в конец.
+//pop - удаляет последний элемент (и возвращает этот элемент!)
+
+let leters = ['a', 'b', 'c', 'd', 'e']
+console.log(leters);
+leters.push('f','s')
+leters.push(1, 55)
+leters.push(88)
+let c = leters.push('g')
+console.log(leters);
+console.log('g');
+
+//
+
+
+let a = leters.pop
+let t = []
+console.log(leters);
+
+
+// String methods: split(), join()
+
+//split - делит строку по указанному разделению, возвращает массив
+//split('') =>  возвращает массив всех символов
+
+let string = 'Hello- World'
+console.log(string);
+
+console.log(string.split(' '));
+let stringNew = string.split('/ ')
+console.log(stringNew);
+
+let newArrayFromString = string.split('')
+console.log(newArrayFromString);
+
+
+
+//join => конвертирует массив в строку, по умолчанию
+//разделяет символы запятой 
+
+let array = ['a', 'b', 'c', 'd', 1, 2, 3]
+console.log(array);
+
+console.log(array.join());
+console.log(array);
+
+let newStringFromArray = array.join(' ')
+console.log(newArrayFromString);

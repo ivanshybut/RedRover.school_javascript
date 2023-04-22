88015a7972714106dd6c84c06fe5d13dcf9a6c91
Ivan Shybut <ivanshybut92@gmail.com>
2023-04-22 22:10:13 +0300

    update async

---
+++ qa_javascript/async.js
@@ -0,0 +1,14 @@
+// Async - это специальный синтаксис , который предназначен для более прстого и удобного написания асинхронного кода. Появился в с ES8. Синтаксис async/await упрощает работу с промисами. Асинхронные функции - это функции которые объявенны с использованием ключеваго слова async.
+
+
+/* Синтаксис:
+async function name (param0) {
+    statements
+}
+async function name (param0, param1){
+    statements
+} 
+async function name (param0, param1, ...., paramN){
+    statements
+}
+*/

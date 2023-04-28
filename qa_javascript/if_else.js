201edc17b0a459f5cc18d83cafd8c07d8fbe5574
Ivan Shybut <ivanshybut92@gmail.com>
2023-04-28 21:36:42 +0300

    if/else update

--- qa_javascript/if_else.js
+++ qa_javascript/if_else.js
@@ -30,3 +30,28 @@ switch (num3) {
     default:
         console.log('Something else');
 }
+console.log('=========================');
+// 
+
+
+let age = 31;
+let price = 10;
+let passengerCategory1 = 'Infants';
+let passengerCategory2 = 'Kids';
+let passengerCategory3 = 'Adults';
+let passengerCategory4 = 'Pensioners';
+
+if (age < 2) {
+    console.log('Ticket price for' + ' ' + passengerCategory1 + ' ' + 'is' + ' USD ' + price * 0.1);
+}
+else if (age >= 2 && age < 18) {
+    console.log('Ticket for price' + ' ' + passengerCategory2 + ' ' + 'is' + ' USD ' + price*0.5);
+
+}
+else if (age >= 18 && age < 65) {
+    console.log('Ticket for price' + ' ' + passengerCategory3 + ' ' + 'is' + ' USD ' + price);
+
+}
+else {
+    console.log('Ticket for price' + ' ' + passengerCategory4 + ' ' + 'is' + ' USD ' + price*0.6);
+}

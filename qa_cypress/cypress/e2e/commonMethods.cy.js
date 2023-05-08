9bb1c4ebd6645ffbb5388cd8dcfe747d9b41e7d9
Ivan Shybut <ivanshybut92@gmail.com>
2023-05-08 22:13:16 +0300

    common methods find

--- qa_cypress/cypress/e2e/commonMethods.cy.js
+++ qa_cypress/cypress/e2e/commonMethods.cy.js
@@ -1,8 +1,10 @@
 /// <reference types ="cypress"/>
 
-describe('First test', () => {
-    beforeEach(() => {
-        cy.visit('https://demoqa.com')
+describe('comon methods', () => {
+    it('verify', () => {
+        cy.visit('https://demoqa.com/')
+        cy.get('div.category-cards .card-body').find('h5').should('have.length', 6)
+        cy.title().should('eq', 'DEMOQA')
     })
 
 })

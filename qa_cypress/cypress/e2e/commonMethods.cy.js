fd7b0007e798664854fcac6bf4d2ccc907c61103
Ivan Shybut <ivanshybut92@gmail.com>
2023-05-09 21:37:46 +0300

    update methods

--- qa_cypress/cypress/e2e/commonMethods.cy.js
+++ qa_cypress/cypress/e2e/commonMethods.cy.js
@@ -5,6 +5,9 @@ describe('comon methods', () => {
         cy.visit('https://demoqa.com/')
         cy.get('div.category-cards .card-body').find('h5').should('have.length', 6)
         cy.title().should('eq', 'DEMOQA')
+        cy.get('div.avatar').should('have.css', 'color', 'rgb(1. 160. 224)')
+        cy.get('div.card-body h5').contains('Element').click()
+        cy.get('div.main-header').should('contain', 'Elements')
     })
 
 })
